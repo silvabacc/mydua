dd066090f137424e350962c976640882bcec08d3
silvabacc <bavernaz@hotmail.com>
2025-04-16 23:22:53 +0100

    drag and drop

--- src/components/TabsContent/CreateDua.tsx
+++ src/components/TabsContent/CreateDua.tsx
@@ -1,61 +1,198 @@
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "../card";
-import { Input } from "../input";
-import { useState } from "react";
-import { Reorder } from "motion/react";
+import { Dispatch, FormEvent, SetStateAction, useState } from "react";
 import duasJson from "../../data/duas.json";
+import { motion } from "framer-motion";
 import { Dua } from "../../types";
-import { Separator } from "../separator";
 
 export default function CreateDua() {
   const [searchedDuas, setSearchedDuas] = useState<Dua[]>(duasJson as Dua[]);
   const [duas, setDuas] = useState<Dua[]>([]);
+  const [cards, setCards] = useState(DEFAULT_CARDS);
 
-  const handleOnDrag = (e: React.DragEvent, dua: Dua) =>
-    e.dataTransfer.setData("dua", JSON.stringify(dua));
-
-  const handleonDrop = (e: React.DragEvent) => {
-    const dua = e.dataTransfer.getData("dua");
-    setDuas((prev) => [...prev, JSON.parse(dua) as Dua]);
-  };
-
-  const handleDragOver = (e: React.DragEvent) => e.preventDefault();
+  console.log(cards);
 
   return (
-    <div className="p-4 space-y-2">
-      <Input placeholder="Search for a dua..." className="" />
-      <div className="grid lg:grid-cols-1 sm:grid-cols-2 gap-4">
-        {searchedDuas.map((dua) => (
-          <Card
-            key={dua.translation}
-            draggable
-            onDragStart={(e) => handleOnDrag(e, dua)}
-          >
-            <CardHeader>
-              <CardTitle>{dua.title}</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <p>{dua.arabic}</p>
-              <p>{dua.translation}</p>
-              <CardFooter>
-                <p>{dua.source}</p>
-              </CardFooter>
-            </CardContent>
-          </Card>
-        ))}
-      </div>
-      <div
-        onDrop={handleonDrop}
-        onDragOver={handleDragOver}
-        className="bg-red-200 h-34"
-      >
-        <Reorder.Group axis="y" values={duas} onReorder={setDuas}>
-          {duas.map((dua) => (
-            <Reorder.Item key={dua.title} value={dua}>
-              {dua.title}
-            </Reorder.Item>
-          ))}
-        </Reorder.Group>
+    <div className="h-screen w-full ">
+      <div className="flex h-full w-full gap-3">
+        <Column column="duas" cards={cards} setCards={setCards} />
+        <Column column="mydua" cards={cards} setCards={setCards} />
       </div>
     </div>
   );
 }
+
+const Board = () => {};
+
+type ColumnProps = {
+  cards: CardType[];
+  column: ColumnType;
+  setCards: Dispatch<SetStateAction<CardType[]>>;
+};
+
+const Column = ({ cards, column, setCards }: ColumnProps) => {
+  const handleDragStart = (e: DragEvent, card: CardType) => {
+    e.dataTransfer.setData("cardId", card.id);
+  };
+
+  const handleDragEnd = (e: DragEvent) => {
+    const cardId = e.dataTransfer.getData("cardId");
+
+    clearHighlights();
+
+    const indicators = getIndicators();
+    const { element } = getNearestIndicator(e, indicators);
+
+    const before = element.dataset.before || "-1";
+
+    if (before !== cardId) {
+      let copy = [...cards];
+
+      let cardToTransfer = copy.find((c) => c.id === cardId);
+      if (!cardToTransfer) return;
+      cardToTransfer = { ...cardToTransfer, column };
+
+      copy = copy.filter((c) => c.id !== cardId);
+
+      const moveToBack = before === "-1";
+
+      if (moveToBack) {
+        copy.push(cardToTransfer);
+      } else {
+        const insertAtIndex = copy.findIndex((el) => el.id === before);
+        if (insertAtIndex === undefined) return;
+
+        copy.splice(insertAtIndex, 0, cardToTransfer);
+      }
+
+      setCards(copy);
+    }
+  };
+
+  const handleDragOver = (e: DragEvent) => {
+    e.preventDefault();
+    highlightIndicator(e);
+  };
+
+  const clearHighlights = (els?: HTMLElement[]) => {
+    const indicators = els || getIndicators();
+
+    indicators.forEach((i) => {
+      i.style.opacity = "0";
+    });
+  };
+
+  const highlightIndicator = (e: DragEvent) => {
+    const indicators = getIndicators();
+
+    clearHighlights(indicators);
+
+    const el = getNearestIndicator(e, indicators);
+
+    el.element.style.opacity = "1";
+  };
+
+  const getNearestIndicator = (e: DragEvent, indicators: HTMLElement[]) => {
+    const DISTANCE_OFFSET = 50;
+
+    const el = indicators.reduce(
+      (closest, child) => {
+        const box = child.getBoundingClientRect();
+
+        const offset = e.clientY - (box.top + DISTANCE_OFFSET);
+
+        if (offset < 0 && offset > closest.offset) {
+          return { offset: offset, element: child };
+        } else {
+          return closest;
+        }
+      },
+      {
+        offset: Number.NEGATIVE_INFINITY,
+        element: indicators[indicators.length - 1],
+      }
+    );
+
+    return el;
+  };
+
+  const getIndicators = () => {
+    return Array.from(
+      document.querySelectorAll(
+        `[data-column="${column}"]`
+      ) as unknown as HTMLElement[]
+    );
+  };
+
+  const handleDragLeave = () => {
+    clearHighlights();
+  };
+
+  const filteredCards = cards.filter((c) => c.column === column);
+
+  return (
+    <div className="w-56 shrink-0">
+      <div
+        onDrop={handleDragEnd}
+        onDragOver={handleDragOver}
+        onDragLeave={handleDragLeave}
+        className={`h-full w-full transition-colors `}
+      >
+        {filteredCards.map((c) => {
+          return <Card key={c.id} {...c} handleDragStart={handleDragStart} />;
+        })}
+        <DropIndicator beforeId={null} column={column} />
+      </div>
+    </div>
+  );
+};
+
+type CardProps = CardType & {
+  handleDragStart: any;
+};
+
+const Card = ({ title, id, column, handleDragStart }: CardProps) => {
+  return (
+    <>
+      <DropIndicator beforeId={id} column={column} />
+      <motion.div
+        layout
+        layoutId={id}
+        draggable="true"
+        onDragStart={(e) => handleDragStart(e, { title, id, column })}
+        className="cursor-grab rounded p-3 active:cursor-grabbing"
+      >
+        <p className="text-sm">{title}</p>
+      </motion.div>
+    </>
+  );
+};
+
+type DropIndicatorProps = {
+  beforeId: string | null;
+  column: string;
+};
+
+const DropIndicator = ({ beforeId, column }: DropIndicatorProps) => {
+  return (
+    <div
+      data-before={beforeId || "-1"}
+      data-column={column}
+      className="my-0.5 h-0.5 w-full bg-violet-400 opacity-0"
+    />
+  );
+};
+
+type ColumnType = "duas" | "mydua";
+
+type CardType = {
+  title: string;
+  id: string;
+  column: ColumnType;
+};
+
+const DEFAULT_CARDS: CardType[] = [
+  // BACKLOG
+  { title: "Look into render bug in dashboard", id: "1", column: "duas" },
+  { title: "SOX compliance checklist", id: "2", column: "duas" },
+  { title: "[SPIKE] Migrate to Azure", id: "3", column: "mydua" },
+  { title: "Document Notifications service", id: "4", column: "mydua" },
+];
